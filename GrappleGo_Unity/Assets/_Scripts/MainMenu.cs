2bd6311ada4d21f88cc5689c68dc8d2d86dd8686
JemuzuSan <siaxiong30a@gmail.com>
2025-09-08 03:26:57 -0700

    Buttons

---
+++ GrappleGo_Unity/Assets/_Scripts/MainMenu.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// Script Creator: Songchalee, James
+/// Recent Date Modified: 09 - 08 - 2025
+
+public class MainMenu : MonoBehaviour
+{
+    //  For PlayButton on MainMenu Scene. //
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    // For QuitButton on MainMenu Scene. //
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+    public void EnterSetting()
+    {
+        SceneManager.LoadScene("SceneMenu");
+    }
+
+    public void ExitSetting()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+
+}
